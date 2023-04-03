eaf62cde7c29579def19671ef7ba4d7212dbc9d4
Simon Owerien <simon.owerien@live.de>
2023-04-04 00:54:24 +0200

    enabled debugging with local launch.json

--- mappings.lua
+++ mappings.lua
@@ -3,26 +3,29 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
-return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+local utils = require "astronvim.utils"
+local is_available = utils.is_available
+
+local maps = { i = {}, n = {}, v = {}, t = {} }
+
+if is_available "nvim-dap" then
+  local launch_debug = function()
+    local json_path = vim.fn.getcwd() .. "/launch.json"
+    if vim.fn.filereadable(json_path) then require("dap.ext.vscode").load_launchjs(json_path) end
+    require("dap").continue()
+  end
+  maps.n["<F5>"] = { launch_debug, desc = "Debugger: Start" }
+  maps.n["<leader>dl"] = { launch_debug, desc = "Start/Continue from launch.json" }
+end
+
+maps.n["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" }
+maps.n["<leader>bD"] = {
+  function()
+    require("astronvim.utils.status").heirline.buffer_picker(
+      function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+    )
+  end,
+  desc = "Pick to close",
 }
+maps.n["<leader>b"] = { name = "Buffers" }
+return maps
