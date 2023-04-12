cdaabcf34f89bd3b4a957b587227c64ae685f73a
Simon Owerien <simon.owerien@live.de>
2023-04-12 18:00:45 +0200

    change

--- plugins/user.lua
+++ plugins/user.lua
@@ -22,4 +22,9 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
   },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function() require("catppuccin").setup() end,
+  },
 }
